b461b842f85773f50d92d5515deff84826464b69
ppr <comercial.luiscestas@gmail.com>
2025-12-18 18:07:55 -0300

    Fix IndexedDB DataError by adding autoIncrement to songs store

--- src/offline/db.js
+++ src/offline/db.js
@@ -1,14 +1,21 @@
 import { openDB } from 'idb';
 
 const DB_NAME = 'MusicPlayerDB';
-const DB_VERSION = 1;
+const DB_VERSION = 2; // Incremented version to apply schema change
 const STORE_NAME = 'songs';
 
 export async function initDB() {
   return openDB(DB_NAME, DB_VERSION, {
-    upgrade(db) {
-      if (!db.objectStoreNames.contains(STORE_NAME)) {
-        db.createObjectStore(STORE_NAME, { keyPath: 'id' });
+    upgrade(db, oldVersion, newVersion) {
+      if (oldVersion < 1) {
+        // Initial creation
+        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
+      } else if (oldVersion < 2) {
+        // Upgrade from version 1 to 2: recreating the store with autoIncrement
+        if (db.objectStoreNames.contains(STORE_NAME)) {
+          db.deleteObjectStore(STORE_NAME);
+        }
+        db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
       }
     },
   });
